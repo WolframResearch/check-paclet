f25e66f4dce6d73b9dd180f38fccb256e922c3f5
Rick Hennigan <richardh@wolfram.com>
2022-01-20 19:37:42 -0500

    testing

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+set -e
+
+export CHECK_PACLET_TARGET=${1};
+export CHECK_PACLET_NOTEBOOK=${2};
+
+wolframscript -code 'PacletInstall["https://wolfr.am/11FhS453R"];PacletInstall["https://wolfr.am/11FhS4xyd"];'
+
+wolframscript -code "PacletResource`CICD`CheckPaclet[]"
