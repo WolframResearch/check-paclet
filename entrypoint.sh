d348c3940d1ec3b72727281c33989e0962aa67f3
Rick Hennigan <richardh@wolfram.com>
2022-01-20 19:49:11 -0500

    one argument for now

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 export CHECK_PACLET_TARGET=${1};
-export CHECK_PACLET_NOTEBOOK=${2};
+# export CHECK_PACLET_NOTEBOOK=${2};
 
 wolframscript -code 'PacletInstall["https://wolfr.am/11FhS453R"];PacletInstall["https://wolfr.am/11FhS4xyd"];'
 
