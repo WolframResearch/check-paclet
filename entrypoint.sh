17b390a62ed998ff9b654047f276fc3ef2e9acaa
Rick Hennigan <richardh@wolfram.com>
2022-01-22 18:07:50 -0500

    Add some status logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 
 export CHECK_PACLET_TARGET=${1};
 # export CHECK_PACLET_NOTEBOOK=${2};
-
+echo "Installing dependencies..."
 wolframscript -code 'PacletInstall["https://wolfr.am/11FhS453R"];PacletInstall["https://wolfr.am/11FhS4xyd"];'
 
+echo "Checking paclet..."
 wolframscript -code 'PacletResource`CICD`CheckPaclet[]'
