e73dee6a8c14378f61ea9789f92d4382a7604aa0
Rick Hennigan <richardh@wolfram.com>
2022-01-24 13:29:19 -0500

    remove redundant echo

--- src/main.sh
+++ src/main.sh
@@ -4,10 +4,6 @@ set -e
 
 export SCRIPT_DIR=$(dirname ${0})
 
-echo "SCRIPT_DIR=${SCRIPT_DIR}"
-echo "INPUT_TARGET=${INPUT_TARGET}"
-echo "DEFINITION_NOTEBOOK=${INPUT_DEFINITION_NOTEBOOK}"
-
 wolframscript ${SCRIPT_DIR}/install_dependencies.wls
 
 wolframscript ${SCRIPT_DIR}/check_paclet.wls
