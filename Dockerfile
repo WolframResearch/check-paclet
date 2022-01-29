37b47e21a1a2dbeb2e41a816c803a189fb0462d0
Rick Hennigan <richardh@wolfram.com>
2022-01-29 16:07:39 -0500

    making formatting consistent between check-paclet and build-paclet

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 # Container image that runs your code
 FROM wolframresearch/wolframengine:13.0.0
-COPY ["src", "~/src/"]
-ENTRYPOINT ["~/src/main.sh"]
+
+COPY ["src", "/src/"]
+
+USER root
+RUN chmod +x /src/main.sh
+
+ENTRYPOINT ["/src/main.sh"]
--- src/main.sh
+++ src/main.sh
@@ -4,6 +4,10 @@ set -e
 
 export SCRIPT_DIR=$(dirname ${0})
 
+echo "::group::Installing dependencies..."
 wolframscript ${SCRIPT_DIR}/install_dependencies.wls
+echo "::endgroup::"
 
+echo "::group::Checking Paclet..."
 wolframscript ${SCRIPT_DIR}/check_paclet.wls
+echo "::endgroup::"
